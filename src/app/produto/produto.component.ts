2928d83fb682b2c49731a0a06513b9f36cbd1aaa
jonatassfredo <jonatassfredo@gmail.com>
2019-10-28 14:46:22 -0300

    ajustes

--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -14,7 +14,6 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProdutoComponent implements OnInit {
   produto: ProdutoModel;
-  // produto: any;
   categorias: Array<CategoriaModel> = new Array<CategoriaModel>();
   produtos: Array<ProdutoModel> = new Array<ProdutoModel>();
 
@@ -23,76 +22,31 @@ export class ProdutoComponent implements OnInit {
     private produtoSrv: ProdutoProvider,
     private router: ActivatedRoute
   ) {
-
-    // this._prod = this.router.snapshot.paramMap.get('_id');
-    // console.log(this._prod);
-
-
-    // if (this._prod) {
-    // this.produto = <ProdutoModel>this._prod;
-    // console.log('descricao', this._prod.descricao);
-    // this.produto.categoriaId = this._prod.categoriaId._id;   
-    // }
-
-
-    // this._prod = this.router.snapshot.paramMap.get('_produto');
-    // console.log(this._prod);
-
-
-    // else this.produto = new ProdutoModel();
-
     this.loadData();
-    // this.load();
   }
 
-  // async load(): Promise<void> {
-  //   try {
-  //     let produtosResult = await this.produtoSrv.produtosById(this._prod);
-  //     if (produtosResult.success)
-  //       this.produtos = <Array<ProdutoModel>>produtosResult.data;
-  //     console.log(produtosResult.data);
-  //   } catch (error) {
-  //     console.log("problema ao carregar os produtos", error);
-  //   }
-  // }
-
   async loadData(): Promise<void> {
+    // Carrega todas as categorias
     try {
       let categoriasResult = await this.categoriaSrv.get();
       if (categoriasResult.success) {
         this.categorias = <Array<CategoriaModel>>categoriasResult.data;
-        console.log('carregando categorias dentro do produto', this.categorias);
-
+        console.log('categorias carregadas', this.categorias);
       }
-    } catch (error) {
+    }
+    catch (error) {
       console.log("Erro ao carregar as categorias", error);
     }
   }
 
   ngOnInit() {
+    // pega os dados do produto vindos da rota
     this.router.params.subscribe((objeto: any) => {
       let prod: ProdutoModel;
       prod = <ProdutoModel>objeto;
       this.produto = prod;
       console.log("produtos ngOnInit", this.produto);
       console.log("categoria ID", prod.categoriaId);
-
-    })
+    });
   }
 }
-
-// let _prod = this.router.snapshot.paramMap.get('_produto');
-// if (_prod && _prod._id) {
-//   this.produto = <ProdutoModel>_prod;
-//   this.produto.categoriaId = _prod.categoriaId._id;
-// } else this.produto = new ProdutoModel();
-
-// let _prod = this.router.paramMap.pipe(
-//   switchMap(params => {
-//     this.produto = params.get('_produto');
-//     return _prod
-
-
-//     // this.loadData();
-//   })
-// );
